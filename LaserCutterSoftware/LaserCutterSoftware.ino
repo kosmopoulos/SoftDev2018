4553bd66e4c3ef40f29c9612eb2cf07f106f5f46
James <kosmopoulosjames@gmail.com>
2018-08-02 17:40:31 +1000

    fixed decimal point issue

--- LaserCutterSoftware/LaserCutterSoftware.ino
+++ LaserCutterSoftware/LaserCutterSoftware.ino
@@ -40,11 +40,46 @@ void setup() {
   Serial.println("setup finished, ready for operation");
   Serial.println("-----------------------------------");
 
-Serial.println(gradient,4);
-float major = 42;
-float minor = 36;
+Serial.println("the initial gradient is:");Serial.println(gradient,4);
+float major = 1500;
+float minor = 1000;
+
+int countmajor, countminor = 0;
+int gradientcounter = 0;
+
 gradient = major/minor;
+
 Serial.println(gradient,4);
+Serial.print("majorcount: ");Serial.println(countmajor);
+Serial.print("minorcount: ");Serial.println(countminor);
+int counterA, counterB = 0;
+
+for (int i = 0; i<major*10; i++)
+{
+  
+counterA++;
+counterB++;
+
+if(counterA >=10)
+{
+   countmajor++;
+   counterA = 0;
+}
+ 
+
+  gradientcounter++;
+  
+  if(gradientcounter>=gradient)
+  {
+    countminor++;
+    gradientcounter = 0;
+  }
+  
+  //Serial.println(i);
+  
+}
+Serial.print("countmajor: ");Serial.println(countmajor);
+Serial.print("countminor: ");Serial.print(countminor);
 
 
 }
