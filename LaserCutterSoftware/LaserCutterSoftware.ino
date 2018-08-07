4344b6d5a65f4470e0de512c39107c85a99c8a4b
James <kosmopoulosjames@gmail.com>
2018-08-07 17:30:41 +1000

    Added second motor functionality

--- LaserCutterSoftware/LaserCutterSoftware.ino
+++ LaserCutterSoftware/LaserCutterSoftware.ino
@@ -1,42 +1,75 @@
-int motor_pin_1 = 2;
-int motor_pin_2 = 3;
-int motor_pin_3 = 4;
-int motor_pin_4 = 5;
+int motorA_pin_1 = 2;
+int motorA_pin_2 = 3;
+int motorA_pin_3 = 4;
+int motorA_pin_4 = 5;
 
+int motorB_pin_1 = 6;
+int motorB_pin_2 = 7;
+int motorB_pin_3 = 8;
+int motorB_pin_4 = 9;
+
+int motorSpeed = 10000;
 void setup() {
   // put your setup code here, to run once:
   
-  pinMode(motor_pin_1, OUTPUT);
-  pinMode(motor_pin_2, OUTPUT);
-  pinMode(motor_pin_3, OUTPUT);
-  pinMode(motor_pin_4, OUTPUT);
+  pinMode(motorA_pin_1, OUTPUT);
+  pinMode(motorA_pin_2, OUTPUT);
+  pinMode(motorA_pin_3, OUTPUT);
+  pinMode(motorA_pin_4, OUTPUT);
+
+  pinMode(motorB_pin_1, OUTPUT);
+  pinMode(motorB_pin_2, OUTPUT);
+  pinMode(motorB_pin_3, OUTPUT);
+  pinMode(motorB_pin_4, OUTPUT);
 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-delayMicroseconds(10000);
-        digitalWrite(motor_pin_1, HIGH);
-        digitalWrite(motor_pin_2, LOW);
-        digitalWrite(motor_pin_3, HIGH);
-        digitalWrite(motor_pin_4, LOW);
+delayMicroseconds(motorSpeed);
+        digitalWrite(motorA_pin_1, HIGH);
+        digitalWrite(motorA_pin_2, LOW);
+        digitalWrite(motorA_pin_3, HIGH);
+        digitalWrite(motorA_pin_4, LOW);
         
-delayMicroseconds(10000);
-        digitalWrite(motor_pin_1, LOW);
-        digitalWrite(motor_pin_2, HIGH);
-        digitalWrite(motor_pin_3, HIGH);
-        digitalWrite(motor_pin_4, LOW);
-delayMicroseconds(10000);
-        digitalWrite(motor_pin_1, LOW);
-        digitalWrite(motor_pin_2, HIGH);
-        digitalWrite(motor_pin_3, LOW);
-        digitalWrite(motor_pin_4, HIGH);
-delayMicroseconds(10000);
-        digitalWrite(motor_pin_1, HIGH);
-        digitalWrite(motor_pin_2, LOW);
-        digitalWrite(motor_pin_3, LOW);
-        digitalWrite(motor_pin_4, HIGH);
+delayMicroseconds(motorSpeed);
+        digitalWrite(motorA_pin_1, LOW);
+        digitalWrite(motorA_pin_2, HIGH);
+        digitalWrite(motorA_pin_3, HIGH);
+        digitalWrite(motorA_pin_4, LOW);
+delayMicroseconds(motorSpeed);
+        digitalWrite(motorA_pin_1, LOW);
+        digitalWrite(motorA_pin_2, HIGH);
+        digitalWrite(motorA_pin_3, LOW);
+        digitalWrite(motorA_pin_4, HIGH);
+delayMicroseconds(motorSpeed);
+        digitalWrite(motorA_pin_1, HIGH);
+        digitalWrite(motorA_pin_2, LOW);
+        digitalWrite(motorA_pin_3, LOW);
+        digitalWrite(motorA_pin_4, HIGH);
+
+        delayMicroseconds(motorSpeed);
+        digitalWrite(motorB_pin_1, HIGH);
+        digitalWrite(motorB_pin_2, LOW);
+        digitalWrite(motorB_pin_3, HIGH);
+        digitalWrite(motorB_pin_4, LOW);
+        
+delayMicroseconds(motorSpeed);
+        digitalWrite(motorB_pin_1, LOW);
+        digitalWrite(motorB_pin_2, HIGH);
+        digitalWrite(motorB_pin_3, HIGH);
+        digitalWrite(motorB_pin_4, LOW);
+delayMicroseconds(motorSpeed);
+        digitalWrite(motorB_pin_1, LOW);
+        digitalWrite(motorB_pin_2, HIGH);
+        digitalWrite(motorB_pin_3, LOW);
+        digitalWrite(motorB_pin_4, HIGH);
+delayMicroseconds(motorSpeed);
+        digitalWrite(motorB_pin_1, HIGH);
+        digitalWrite(motorB_pin_2, LOW);
+        digitalWrite(motorB_pin_3, LOW);
+        digitalWrite(motorB_pin_4, HIGH);
      
   
 }
