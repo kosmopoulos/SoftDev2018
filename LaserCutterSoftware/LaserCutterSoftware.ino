e8f8876aa0b43457c53a80f30b5c612ef8e72324
James <kosmopoulosjames@gmail.com>
2018-08-07 19:26:22 +1000

    Added comments, started work on for loop to create movement in steps

--- LaserCutterSoftware/LaserCutterSoftware.ino
+++ LaserCutterSoftware/LaserCutterSoftware.ino
@@ -1,26 +1,117 @@
-int motorA_pin_1 = 2;
-int motorA_pin_2 = 3;
-int motorA_pin_3 = 4;
-int motorA_pin_4 = 5;
-
-int motorB_pin_1 = 6;
-int motorB_pin_2 = 7;
-int motorB_pin_3 = 8;
-int motorB_pin_4 = 9;
-
+//Sets pins 2,3,4,5 for motor A
+int motorPinA1 = 2;
+int motorPinA2 = 3;
+int motorPinA3 = 4;
+int motorPinA4 = 5;
+//Sets pins 6,7,8,9 for motor B
+int motorPinB1 = 6;
+int motorPinB2 = 7;
+int motorPinB3 = 8;
+int motorPinB4 = 9;
+//Sets initial value of step count for both motors to 0
+int stepA, stepB = 0;
+//Sets the speed of the stepper motors
 int motorSpeed = 10000;
-void setup() {
-  // put your setup code here, to run once:
-  
-  pinMode(motorA_pin_1, OUTPUT);
-  pinMode(motorA_pin_2, OUTPUT);
-  pinMode(motorA_pin_3, OUTPUT);
-  pinMode(motorA_pin_4, OUTPUT);
+//Initialises the motor to select, how many steps of the motor and the direction
+int motorSelect, steps, motorDirection;
+//Initialises the function to allow for the motor to move and its inputs
+void motorMove(int motorSelect, int steps, int motorDirection);
+int j;
 
-  pinMode(motorB_pin_1, OUTPUT);
-  pinMode(motorB_pin_2, OUTPUT);
-  pinMode(motorB_pin_3, OUTPUT);
-  pinMode(motorB_pin_4, OUTPUT);
+//Defines the function to move the motors
+void motorMove(int motorSelect, int steps, int motorDirection) {
+  //Checks direction of motor movement
+  if(motorDirection >= 1) {
+    // for loop for how many steps
+    for(int j; j < steps; j++) {
+      //Case statements to cycle through phases of a single step in the motor
+      switch (stepA) {
+          case 0:
+          digitalWrite(2, HIGH);
+          digitalWrite(3, LOW);
+          digitalWrite(4, HIGH);
+          digitalWrite(5, LOW);
+          stepA ++;
+          break;
+          
+          case 1:
+          digitalWrite(2, LOW);
+          digitalWrite(3, HIGH);
+          digitalWrite(4, HIGH);
+          digitalWrite(5, LOW);
+          stepA ++;
+          break;    
+              
+          case 2:
+          digitalWrite(2, LOW);
+          digitalWrite(3, HIGH);
+          digitalWrite(4, LOW);
+          digitalWrite(5, HIGH);
+          stepA ++;
+          break;  
+               
+          case 3:
+          digitalWrite(2, HIGH);
+          digitalWrite(3, LOW);
+          digitalWrite(4, LOW);
+          digitalWrite(5, HIGH);
+          stepA = 0;
+          break;
+          }
+    }
+  }
+  else if(motorDirection <= -1) {
+    // for loop for how many steps
+    for(int j; j < steps; j++) {
+      //Case statements to cycle through phases of a single step in the motor
+      switch (stepA) {
+          case 0:
+          digitalWrite(2, HIGH);
+          digitalWrite(3, LOW);
+          digitalWrite(4, HIGH);
+          digitalWrite(5, LOW);
+          stepA --;
+          break;
+          
+          case 1:
+          digitalWrite(2, LOW);
+          digitalWrite(3, HIGH);
+          digitalWrite(4, HIGH);
+          digitalWrite(5, LOW);
+          stepA --;
+          break;    
+              
+          case 2:
+          digitalWrite(2, LOW);
+          digitalWrite(3, HIGH);
+          digitalWrite(4, LOW);
+          digitalWrite(5, HIGH);
+          stepA --;
+          break;  
+               
+          case 3:
+          digitalWrite(2, HIGH);
+          digitalWrite(3, LOW);
+          digitalWrite(4, LOW);
+          digitalWrite(5, HIGH);
+          stepA = 0;
+          break;
+          }
+    }
+}
+}
+
+void setup() {
+  //Sets the pins for motor A to send output signals
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  //Sets the pins for motor B to send output signals
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
 
 
 }
@@ -28,48 +119,29 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 delayMicroseconds(motorSpeed);
-        digitalWrite(motorA_pin_1, HIGH);
-        digitalWrite(motorA_pin_2, LOW);
-        digitalWrite(motorA_pin_3, HIGH);
-        digitalWrite(motorA_pin_4, LOW);
-        
-delayMicroseconds(motorSpeed);
-        digitalWrite(motorA_pin_1, LOW);
-        digitalWrite(motorA_pin_2, HIGH);
-        digitalWrite(motorA_pin_3, HIGH);
-        digitalWrite(motorA_pin_4, LOW);
-delayMicroseconds(motorSpeed);
-        digitalWrite(motorA_pin_1, LOW);
-        digitalWrite(motorA_pin_2, HIGH);
-        digitalWrite(motorA_pin_3, LOW);
-        digitalWrite(motorA_pin_4, HIGH);
-delayMicroseconds(motorSpeed);
-        digitalWrite(motorA_pin_1, HIGH);
-        digitalWrite(motorA_pin_2, LOW);
-        digitalWrite(motorA_pin_3, LOW);
-        digitalWrite(motorA_pin_4, HIGH);
+
 
         delayMicroseconds(motorSpeed);
-        digitalWrite(motorB_pin_1, HIGH);
-        digitalWrite(motorB_pin_2, LOW);
-        digitalWrite(motorB_pin_3, HIGH);
-        digitalWrite(motorB_pin_4, LOW);
-        
+        digitalWrite(6, HIGH);
+        digitalWrite(7, LOW);
+        digitalWrite(8, HIGH);
+        digitalWrite(9, LOW);
+
 delayMicroseconds(motorSpeed);
-        digitalWrite(motorB_pin_1, LOW);
-        digitalWrite(motorB_pin_2, HIGH);
-        digitalWrite(motorB_pin_3, HIGH);
-        digitalWrite(motorB_pin_4, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, HIGH);
+        digitalWrite(8, HIGH);
+        digitalWrite(9, LOW);
 delayMicroseconds(motorSpeed);
-        digitalWrite(motorB_pin_1, LOW);
-        digitalWrite(motorB_pin_2, HIGH);
-        digitalWrite(motorB_pin_3, LOW);
-        digitalWrite(motorB_pin_4, HIGH);
+        digitalWrite(6, LOW);
+        digitalWrite(7, HIGH);
+        digitalWrite(8, LOW);
+        digitalWrite(9, HIGH);
 delayMicroseconds(motorSpeed);
-        digitalWrite(motorB_pin_1, HIGH);
-        digitalWrite(motorB_pin_2, LOW);
-        digitalWrite(motorB_pin_3, LOW);
-        digitalWrite(motorB_pin_4, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(7, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(9, HIGH);
      
   
 }
