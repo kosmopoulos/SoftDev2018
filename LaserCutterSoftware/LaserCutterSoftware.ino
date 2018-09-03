facd2674621706c4dde51fd42898d736cbe0160a
James <kosmopoulosjames@gmail.com>
2018-09-03 21:23:00 +1000

    Joystick movement working completely and cleaned a lot of code

--- LaserCutterSoftware/LaserCutterSoftware.ino
+++ LaserCutterSoftware/LaserCutterSoftware.ino
@@ -5,14 +5,14 @@ int savedAddress;
 //Initiates laser pin
 int ledPin =  13;
 //Joystick variables for each direction
-int buttonLeft, buttonRight, buttonUp, buttonDown = 0;
+int joystickLeft, joystickRight, joystickUp, joystickDown = 0;
 //Joystick variables used to set the initial state as off
-int buttonState = 0;
-int buttonState2 = 0;  
-int buttonState3 = 0;  
-int buttonState4 = 0;  
+int joystickState = 0;
+int joystickState2 = 0;  
+int joystickState3 = 0;  
+int joystickState4 = 0;  
 
-
+//Creates variables for menu use
 int incomingSerial = 0;
 int proceed = 0;
 int menuItem = 0;
@@ -52,11 +52,12 @@ float gradientY;
 void movePos(int deltaX, int deltaY);
 
 //Initialises the function to operate the laser
-void laserEngrave(int laserBrightness);
+void laserEngrave();
 
 //Initialises function to turn laser off
 void laserOff();
 void laserOn();
+int laserPin = 11;
 //Initialises the function to move to a specific position
 void moveCoord(float posX, float posY);
 
@@ -69,7 +70,7 @@ void saveCoord();
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
+  // initialize the joystick switches as inputs:
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
@@ -91,104 +92,101 @@ void setup() {
 }
 
 void loop() {
-digitalWrite(11, HIGH);
-Serial.println("welcome to the program");
-Serial.println("what would you like to do?");
-Serial.println("enter 1 for this, 2 for that, or 3 for random inputs");
-
-////////////////////////////////////////////////////////////////////
-while(proceed == 0)
-{
-if (Serial.available()) {
-incomingSerial = Serial.read();
-switch(incomingSerial)
-{
-  default: Serial.println("please make sure you enter either 1, 2 or 3");
-  break;
-
+  Serial.println("LASER ENGRAVER");
+  Serial.println("Welcome, what would you like to do?");
+  Serial.println("Enter 1 for demo mode, 2 for Saving and reading or 3 for joystick control");
   
-  case 49:
-  //go to one situation
-  Serial.print("number 1 was received");
-  proceed = 1;
-  menuItem = 1;
-
-  break;
-  case 50:
-  //go to two situation
-  Serial.print("number 2 was received");
- proceed = 1;
- menuItem = 2;
-  break;
-  case 51:
-  //go to random movement mode
-  Serial.print("number 3 was received");
- proceed = 1;
- menuItem = 3;
-  break;
-}
-}
-
-
-}
-proceed = 0;
-//////////////////////////////////////////////////////////////////////Joystick movement
-Serial.println("Joystick mode, use the joystick to control the laser");
-//Checks if the selected option was joystick movement
-if (menuItem == 3){
+  ////////////////////////////////////////////////////////////////////
   while(proceed == 0) {
-    //Reads the input of the joystick for each direction
-    buttonLeft = digitalRead(A0);
-    buttonRight = digitalRead(A1);
-    buttonUp = digitalRead(A2);
-    buttonDown = digitalRead(A3);
-    //Logs the inputs of the joystick to the serial monitor, 0 for no signal and 1 for signal
-    Serial.print(buttonLeft);
-    Serial.print("     ");
-    Serial.print(buttonRight);
-    Serial.print("     ");
-    Serial.print(buttonUp);
-    Serial.print("     ");
-    Serial.println(buttonDown);
-    //Checks to see if there is an input, and then moves laser in according direction
-    if(buttonRight) {movePos(100,0);}
-    if(buttonLeft) {movePos(-100,0);}
-    if(buttonUp) {movePos(0,100);}
-    if(buttonDown) {movePos(0,-100);}
-    if(buttonUp && buttonRight) {movePos(100,100);}
-    if(buttonUp && buttonLeft) {movePos(-100,100);}
-    if(buttonDown && buttonRight) {movePos(100,-100);}
-    if(buttonDown && buttonLeft) {movePos(-100,-100);}
+    if (Serial.available()) {
+      incomingSerial = Serial.read();
+      menuItem = 0;
+      if(menuItem == 0) {
+        switch(incomingSerial) {
+          default: Serial.println("Please ensure that you choose either 1, 2 or 3. I promise that there aren't any hidden options");
+          break;
+          //Switches to demo mode
+          case 49:
+          Serial.println("number 1 was received");
+          proceed = 1;
+          menuItem = 1;  
+          break;
+          //Switches to save/read mode
+          case 50:
+          Serial.println("number 2 was received");
+          proceed = 1;
+          menuItem = 2;
+          break;
+          //Switches to joystick movement mode
+          case 51:
+          Serial.println("number 3 was received");
+          proceed = 1;
+          menuItem = 3;
+          break;
+        }
+      }  
+    }
   }
-}
-/////////////////////////////////////////////////////EEPROM saving and reading
-if (menuItem == 2){
-  while(proceed == 0) {
-    Serial.println("Coord saving and reading screen, choose if you want to save or read");
-        
+  proceed = 0;
+  //////////////////////////////////////////////////////////////////////Joystick movement
+    Serial.println("Joystick mode, use the joystick to control the laser");
+    //Checks if the selected option was joystick movement
+    if (menuItem == 3){
+      while(proceed == 0) {
+        //Reads the input of the joystick for each direction
+        joystickLeft = digitalRead(A0);
+        joystickRight = digitalRead(A1);
+        joystickUp = digitalRead(A2);
+        joystickDown = digitalRead(A3);
+        //Logs the inputs of the joystick to the serial monitor, 0 for no signal and 1 for signal
+        Serial.print(joystickLeft);
+        Serial.print("     ");
+        Serial.print(joystickRight);
+        Serial.print("     ");
+        Serial.print(joystickUp);
+        Serial.print("     ");
+        Serial.println(joystickDown);
+        //Checks to see if there is an input, and then moves laser in according direction
+        if(joystickRight && !joystickLeft && !joystickUp && !joystickDown) {movePos(100,0); digitalWrite(11, HIGH);}
+        if(joystickLeft && !joystickRight && !joystickDown && !joystickUp) {movePos(-100,0); digitalWrite(11, HIGH);}
+        if(joystickUp && !joystickRight && !joystickDown && !joystickLeft) {movePos(0,100); digitalWrite(11, HIGH);}
+        if(joystickDown && !joystickUp && !joystickRight && !joystickLeft) {movePos(0,-100); digitalWrite(11, HIGH);}
+        if(joystickUp && joystickRight && !joystickDown && !joystickLeft) {moveCoord(10,10); digitalWrite(11, HIGH);}
+        if(joystickUp && joystickLeft && !joystickDown && !joystickRight) {moveCoord(-10,10); digitalWrite(11, HIGH);}
+        if(joystickDown && joystickRight && !joystickUp && !joystickLeft) {moveCoord(10,-10); digitalWrite(11, HIGH);}
+        if(joystickDown && joystickLeft && !joystickUp && !joystickRight) {moveCoord(-10,-10); digitalWrite(11, HIGH);}
+        //Lowers laser intensity if no movement of axis
+        if(!joystickDown && !joystickLeft && !joystickUp && !joystickRight) {digitalWrite(11, LOW);}
+      }
+    }
+  /////////////////////////////////////////////////////EEPROM saving and reading
+  if (menuItem == 2){
+    while(proceed == 0) {
+      Serial.println("Coord saving and reading screen, choose if you want to save or read");
+          
+    }
   }
-}
-/////////////////////////////////////////////////////Demo code
-if (menuItem == 1){
-  while(proceed == 0) {
-    Serial.println("Welcome to demo mode! The system will demo the laser engraving ability");
-    Serial.println("Press x to go back");
-    delay(100);
-    //Square demo
-    moveCoord(20,0);
-    moveCoord(0,20);
-    moveCoord(-20,0);
-    moveCoord(0,-20);
-    //Triangle demo
-    moveCoord(26,0);
-    moveCoord(-13,26);
-    moveCoord(-13,-26); 
-  }
-
-}
-  Serial.println("we proceeded");
-
-
-
-  while(1) {}
+  /////////////////////////////////////////////////////Demo code
+    if (menuItem == 1){
+      while(proceed == 0) {
+        Serial.println("Welcome to demo mode! The system will demo the laser engraving ability");
+        Serial.println("Enter x to go back");
+        delayMicroseconds(100);
+        digitalWrite(11, HIGH);
+        //Square demo
+        moveCoord(20,0);
+        moveCoord(0,20);
+        moveCoord(-20,0);
+        moveCoord(0,-20);
+        delayMicroseconds(100);
+        //Triangle demo
+        moveCoord(26,0);
+        moveCoord(-13,26);
+        moveCoord(-13,-26); 
+        digitalWrite(11, LOW);
+        if(incomingSerial == 120) {proceed = 0;}
+      }
+    
+    }
+    while(1) {}
 }
--- LaserCutterSoftware/LaserFunctions.ino
+++ LaserCutterSoftware/LaserFunctions.ino
@@ -1,15 +1,10 @@
-////Defines the function to use the laser
-//void laserEngrave(int laserBrightness) {
-//  //Maps out brightness from 0-255 to 0-100 to obtain as a percentage then sends signal to turn on laser
-//  map(laserBrightness, 0, 100, 0, 255);
-//  analogWrite(laserPin, laserBrightness);
-//}
-//
-//void laserOff() {
-//  digitalWrite(laserPin, LOW);
-//  Serial.println("dsf");
-//}
-//
-//void laserOn() {
+//////Defines the function to use the laser
+//void laserEngrave() {
 //  digitalWrite(laserPin, HIGH);
 //}
+////
+//void laserOff() {
+//  digitalWrite(laserPin, LOW);
+//  Serial.println("Laser off");
+//}
+
